a10c9165f6939f673ea9d0b40ed7873fb1c1ea17
qiulanzhu <178673693@qq.com>
2019-11-02 20:08:45 +0800

    first commit.

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "row": 10,
+    "column": 10,
+    "startStrategyNum": 200,
+    "conditionNum": 3 * 3 * 3 * 3 * 3,
+    "actions": [0, 1, 2, 3, 4, 5, 6],
+    "taskNum": 100,
+    "stepNum": 200,
+    "genNum": 500
+};
